9535ff3489faac950cca3d4e79f743a356aa6158
Jeff Arce (MacBook Air) <jeffandrew.s.arce@gmail.com>
2020-11-07 18:12:44 +0800

    Update Recipes Page

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { RecipesService } from './recipes.service';
 
 @Component({
   selector: 'app-recipes',
@@ -7,24 +8,13 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
-
-  recipes: Recipe[] = [{
-    id: 'r1',
-    title: 'Schnitzel',
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Schnitzel.JPG/1024px-Schnitzel.JPG',
-    ingredients: ['French Fries', 'Lemon', 'Vegetable']
-  },
-  {
-    id: 'r2',
-    title: 'Spaghetti',
-    imageUrl: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/homemade-spaghetti-sauce-horizontal-1530890913.jpg',
-    ingredients: ['Pasta', 'Meat', 'Tomatoes']
-  }
-  ];
   
-  constructor() { }
+  recipes: Recipe[];
+  
+
+  constructor(private recipesService: RecipesService) { }
 
   ngOnInit() {
+    this.recipes = this.recipesService.getAllRecipes();
   }
-
 }
