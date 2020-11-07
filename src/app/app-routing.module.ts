2f4add7bf92d2ca0bf88ea04baf71eadfaf3ed9a
Jeff Arce (MacBook Air) <jeffandrew.s.arce@gmail.com>
2020-11-07 17:53:18 +0800

    Update routes in app-routing Module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'recipes',
-    pathMatch: 'full'
-  },
+  { path: '', redirectTo: 'recipes', pathMatch: 'full' },
   {
     path: 'recipes',
-    loadChildren: () => 
-      import('./recipes/recipes.module').then( m => m.RecipesPageModule)
-  },
+    children: [
+      {
+        path: '',
+        loadChildren: './recipes/recipes.module#RecipesPageModule'    
+      },
+      {
+        path: ':recipeId',
+        loadChildren:
+          './recipes/recipe-detail/recipe-detail.module#RecipeDetailPageModule'
+      }
+    ]
+  }
 ];
 
 @NgModule({
