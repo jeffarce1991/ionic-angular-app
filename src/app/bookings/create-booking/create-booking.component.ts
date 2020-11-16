d9cbb26960d064f1fee0e586e6aba9262c6d1eab
Jeff Arce (MacBook Air) <jeffandrew.s.arce@gmail.com>
2020-11-16 10:37:01 +0800

    Update Modal html and functions.

--- src/app/bookings/create-booking/create-booking.component.ts
+++ src/app/bookings/create-booking/create-booking.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { Place } from 'src/app/places/place.model';
 
 @Component({
   selector: 'app-create-booking',
@@ -6,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-booking.component.scss'],
 })
 export class CreateBookingComponent implements OnInit {
+  @Input() selectedPlace: Place;
 
-  constructor() { }
+  constructor(private modalController: ModalController) { }
 
   ngOnInit() {}
+
+  onCancel() {
+    this.modalController.dismiss(null, 'cancel');
+  }
+
+  onBookPlace() {
+    this.modalController.dismiss(
+      {message: "This is dummy message"},
+      'confirm'
+    );
+  }
+
 
 }
