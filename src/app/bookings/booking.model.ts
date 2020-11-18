43133d273623a12bce329c99e68ce4082564eee8
Jeff Arce (MacBook Air) <jeffandrew.s.arce@gmail.com>
2020-11-18 22:15:37 +0800

    Add Booking modal.

---
+++ src/app/bookings/booking.model.ts
@@ -0,0 +1,12 @@
+import { StringMap } from '@angular/compiler/src/compiler_facade_interface';
+
+export class Booking {
+    constructor(
+        public id: string,
+        public placeId: string,
+        public userId: string,
+        public placeTitle: string,
+        public guestNumber: string) {
+
+    }
+}
